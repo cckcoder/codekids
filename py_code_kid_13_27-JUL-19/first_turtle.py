562063447b26ebf72a6aa16ba3e5fe3446ebbd8d
codewiz <c.chaiwat.ch@gmail.com>
2020-10-03 22:02:15 +0700

    update: curriculum
    Signed-off-by: codewiz <c.chaiwat.ch@gmail.com>

--- py_code_kid_13_27-JUL-19/first_turtle.py
+++ py_code_kid_13_27-JUL-19/first_turtle.py
@@ -1,8 +1,9 @@
-# import turtle
-# t = turtle.Pen()
-# t.speed(0)
-# for x in range(400):
-#   t.forward(x)
-#   t.left(90)
+import turtle
+
+t = turtle.Pen()
+t.speed(0)
+for x in range(400):
+    t.forward(x)
+    t.left(90)
 
 print("Hello world")
--- py_code_kid_13_27-JUL-19/turtle.py
+++ py_code_kid_13_27-JUL-19/turtle.py
@@ -1,4 +1,5 @@
 import turtle
+
 my_window = turtle.Screen()
 my_window.bgcolor("blue")       # creates a graphics window
 my_pen = turtle.Turtle()
